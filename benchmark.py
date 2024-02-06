8c6df06b044954fda9a0905d6a5db58951112529
pjjajal <jajalpurvish@gmail.com>
2024-02-06 18:34:31 -0500

    Added ablation benchmark, made CLI cleaner.

--- benchmark.py
+++ benchmark.py
@@ -4,20 +4,28 @@ import warnings
 warnings.filterwarnings(action="ignore", category=UserWarning)
 
 import argparse
-from typing import Any, Literal
+import itertools
+import textwrap
+from typing import Any, Literal, List, Dict
 
 import torch
 import torch.utils.benchmark as bench
+import pandas as pd
 
 from compvit.factory import compvit_factory
 from dinov2.factory import dinov2_factory
 
-import textwrap
-
 
 def parse_args():
     parser = argparse.ArgumentParser("Benchmarking Code")
-    parser.add_argument(
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--device", choices=["cuda", "cpu", "mps"])
+
+    single_group = parser.add_argument_group(
+        title="Benchmark Single",
+        description="Use these arguements to benchmark a SINGLE model.",
+    )
+    single_group.add_argument(
         "--model",
         choices=[
             "dinov2_vittiny14",
@@ -31,10 +39,33 @@ def parse_args():
             "compvitg14",
         ],
     )
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--device", choices=["cuda", "cpu", "mps"])
-    parser.add_argument("--all-compvit", action="store_true")
-    parser.add_argument("--all-dino", action="store_true")
+
+    benchmark_all_group = parser.add_argument_group(
+        title="Benchmark All",
+        description="Use these flags to benchmark a class of models.",
+    )
+    benchmark_all_group.add_argument("--all-compvit", action="store_true")
+    benchmark_all_group.add_argument("--all-dino", action="store_true")
+
+    sweep_group = parser.add_argument_group(
+        title="CompViT Sweep", description="Use these arguments to ablate CompViT."
+    )
+    sweep_group.add_argument(
+        "--sweep-model",
+        choices=[
+            "compvits14",
+            "compvitb14",
+            "compvitl14",
+            "compvitg14",
+        ],
+    )
+    sweep_group.add_argument("--compvit-sweep", action="store_true")
+    sweep_group.add_argument(
+        "--compressed-tokens-sweep", nargs="+", type=int, dest="token_sweep"
+    )
+    sweep_group.add_argument(
+        "--bottleneck-loc-sweep", nargs=2, type=int, dest="bottleneck_locs"
+    )
 
     return parser.parse_args()
 
@@ -73,10 +104,14 @@ def colour_text(
 
 def print_device_info(args):
     device = torch.device(args.device)
+    device_name = ""
     if device.type == "cuda":
         device_name = torch.cuda.get_device_name(0)
         print(f"{colour_text('Device', 'red')}: {device_name}")
+    return device_name
 
+def export_sweep_data(data: List[Dict[str, Any]], filename):
+    pd.DataFrame(data).to_csv(filename)
 
 ### Create a benchmark function (very simple)
 def benchmark_compvit_milliseconds(x: torch.Tensor, model: torch.nn.Module) -> Any:
@@ -181,6 +216,85 @@ def test_compvit(args):
         print(message)
 
 
+def compvit_sweep(args):
+    model_name = args.sweep_model
+
+    ### Get args, device
+    device = torch.device(args.device)
+
+    token_sweep_iter = [None]
+    bottleneck_locs_iter = [None]
+    if args.token_sweep:
+        token_sweep = args.token_sweep
+        token_sweep_iter = token_sweep
+    if args.bottleneck_locs:
+        start, end = args.bottleneck_locs
+        bottleneck_locs_iter = range(start, end + 1)
+
+    all_data = []
+    for bottleneck_loc, compressed_tokens in itertools.product(
+        bottleneck_locs_iter, token_sweep_iter
+    ):
+        if bottleneck_loc and compressed_tokens:
+            model, config = compvit_factory(
+                model_name=model_name,
+                num_compressed_tokens=compressed_tokens,
+                bottleneck_locs=[bottleneck_loc, bottleneck_loc + 1],
+            )
+        elif bottleneck_loc:
+            model, config = compvit_factory(
+                model_name=model_name,
+                bottleneck_locs=[bottleneck_loc, bottleneck_loc + 1],
+            )
+        elif compressed_tokens:
+            model, config = compvit_factory(
+                model_name=model_name,
+                num_compressed_tokens=compressed_tokens,
+            )
+
+        model = model.to(device).eval()
+        latency_mean, latency_median, latency_iqr = inference(
+            model, device, args.batch_size
+        )
+
+        message = f"""\
+        ========================
+        {colour_text(model_name.upper(), 'green')}
+        {colour_text("Parameters", 'cyan')}: {sum(p.numel() for p in model.parameters()):,}
+        {colour_text("Depth", 'cyan')}: {config['depth']}
+        {colour_text("Embedding Dim", 'cyan')}: {config['embed_dim']}
+        {colour_text("num_compressed_tokens", 'cyan')}: {config['num_compressed_tokens']}
+        {colour_text("bottleneck_locs", 'cyan')}: {config['bottleneck_locs']}
+        {colour_text("bottleneck_size", 'cyan')}: {config['bottleneck_size']}
+        {colour_text("bottleneck", 'cyan')}: {config['bottleneck']}
+        {colour_text("Mean (ms)", "magenta")}: {latency_mean:.2f}
+        {colour_text("Median (ms)", "magenta")}: {latency_median:.2f}
+        {colour_text("IQR (ms)", "magenta")}: {latency_iqr:.2f}
+        ========================\
+        """
+        message = textwrap.dedent(message)
+
+        print(message)
+
+        all_data.append(
+            {
+                "Parameters": sum(p.numel() for p in model.parameters()),
+                "Depth": config["depth"],
+                "Embedding Dim": config["embed_dim"],
+                "num_compressed_tokens": config["num_compressed_tokens"],
+                "bottleneck_locs": config["bottleneck_locs"],
+                "bottleneck_size": config["bottleneck_size"],
+                "bottleneck": config["bottleneck"],
+                "Mean (ms)": latency_mean,
+                "Median (ms)": latency_median,
+                "IQR (ms)": latency_iqr,
+            }
+        )
+    
+    filename = "_".join([print_device_info(args).replace(" ", "_"), model_name, str(args.batch_size)]) + ".csv"
+    export_sweep_data(all_data, filename)
+
+
 def test_single(args):
     ### Get args, device
     device = torch.device(args.device)
@@ -210,20 +324,32 @@ def test_single(args):
 
 def main():
     args = parse_args()
-
     print_device_info(args)
 
     testing_multiple = args.all_dino or args.all_compvit
     if testing_multiple:
         if args.all_dino:
-            print(f"{colour_text(f'Benchmarking DINOv2 Models @ batch size = {args.batch_size}.', 'yellow')}")
+            print(
+                f"{colour_text(f'Benchmarking DINOv2 Models @ batch size = {args.batch_size}.', 'yellow')}"
+            )
             test_dino(args)
         if args.all_compvit:
-            print(f"{colour_text(f'Benchmarking CompViT Models  @ batch size = {args.batch_size}.', 'yellow')}")
+            print(
+                f"{colour_text(f'Benchmarking CompViT Models  @ batch size = {args.batch_size}.', 'yellow')}"
+            )
             test_compvit(args)
         return 0
-
-    test_single(args)
+    elif args.compvit_sweep:
+        message = f"""\
+        {colour_text(f'Benchmarking CompViT Models  @ batch size = {args.batch_size}.', 'yellow')}
+        {colour_text(f"Sweeping Compressed Tokens {args.token_sweep}.", 'yellow')}
+        {colour_text(f"Sweeping Bottleneck Locations from {args.bottleneck_locs}.", "yellow")}\
+        """
+        print(textwrap.dedent(message))
+        compvit_sweep(args)
+        return 0
+    else:
+        test_single(args)
     return 0
 
 
